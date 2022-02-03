1c6f71ad5267bdf67a6347e801b3554478fbb4ef
Adam Churnside <adam.churnside@gmail.com>
2022-02-02 18:31:42 -0800

    Added material UI, qrcode and react-qr-reader

--- src/App.js
+++ src/App.js
@@ -1,25 +1,141 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import {
+  Container,
+  Card,
+  CardContent,
+  makeStyles,
+  Grid,
+  TextField,
+  Button,
+} from "@material-ui/core";
+import QRCode from "qrcode";
+import QrReader from "react-qr-reader";
 
 function App() {
+  const [text, setText] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [ScanResultFile, setScanResultFile] = useState("");
+  const [ScanResultWebCam, setScanResultWebCam] = useState("");
+  const classes = useStyles();
+  const qrRef = useRef(null);
+
+  const generateQrCode = async () => {
+    try {
+      const response = await QRCode.toDataURL(text);
+      setImageUrl(response);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleErrorFile = (error) => {
+    console.log(error);
+  };
+
+  const handleScanFile = (result) => {
+    if (result) {
+      setScanResultFile(result);
+    }
+  };
+
+  const onScanFile = () => {
+    qrRef.current.openImageDialog();
+  };
+
+  const handleErrorWebCam = (error) => {
+    console.log(error);
+  };
+  const handleScanWebCam = (result) => {
+    if (result) {
+        setScanResultWebCam(result)
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className={classes.container}>
+      <Card>
+        <h2 className={classes.title}>
+          Generate, Download &amp; Scan QR code with React JS
+        </h2>
+        <CardContent>
+          <Grid container spacing={2}>
+            <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
+              <TextField
+                label="Enter Text Here"
+                onChangeCapture={(e) => setText(e.target.value)}
+              ></TextField>
+              <Button
+                className={classes.btn}
+                variant="contained"
+                color="primary"
+                onClick={() => generateQrCode()}
+              >
+                Generate
+              </Button>
+              <p></p>
+              {imageUrl ? (
+                <a href={imageUrl} download>
+                  <img
+                    src={imageUrl}
+                    alt="QR Code"
+                    className={classes.qrCode}
+                  />
+                </a>
+              ) : null}
+            </Grid>
+            <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
+              <Button
+                className={classes.btn}
+                variant="contained"
+                color="secondary"
+                onClick={onScanFile}
+              >
+                Scan Qr Code
+              </Button>
+              <QrReader
+                ref={qrRef}
+                delay={300}
+                style={{ width: "100%" }}
+                onError={handleErrorFile}
+                onScan={handleScanFile}
+                legacyMode
+              />
+              <h3>Scanned Code: {ScanResultFile}</h3>
+            </Grid>
+            <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
+              <h3>Qr code scan by web cam</h3>
+              <QrReader
+                delay={300}
+                style={{ width: "100%" }}
+                onError={handleErrorWebCam}
+                onScan={handleScanWebCam}
+              />
+              <h3>Scanned By WebCam Code: {ScanResultWebCam}</h3>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    </Container>
   );
 }
-
+const useStyles = makeStyles((theme) => ({
+  container: {
+    marginTop: 10,
+  },
+  title: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    background: "#5435b7",
+    color: "#fff",
+    padding: 20,
+  },
+  btn: {
+    background: "#5435b7",
+    marginTop: 10,
+    marginBottom: 20,
+    marginLeft: 5,
+  },
+}));
 export default App;
